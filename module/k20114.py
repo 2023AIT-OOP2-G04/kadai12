47f89657da255bddd1aca51d01f5decc066f155c
ait-MM <k20114kk@aitech.ac.jp>
2024-01-18 15:23:06 +0900

    docs

--- module/k20114.py
+++ module/k20114.py
@@ -1,27 +1,7 @@
 import cv2
 import numpy as np
 import os
-from abc import ABCMeta, abstractmethod
-
-class PostProcessing(metaclass=ABCMeta):
-
-    def __init__(self):
-        project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        self.editFolderPath = os.path.join(project_root, "img", "edit") ##jpeg→editに
-        self.outputFolderPath = os.path.join(project_root, "img", "saved")##jpeg→savedに
-
-        os.makedirs(self.editFolderPath, exist_ok=True)
-        os.makedirs(self.outputFolderPath, exist_ok=True)
-
-    def getEditImage(self) -> np.ndarray:
-        for fileName in os.listdir(self.editFolderPath):
-            if fileName.endswith(('.png', '.jpg', '.jpeg', '.gif')):
-                filePath = os.path.join(self.editFolderPath, fileName)
-                img = cv2.imread(filePath, cv2.IMREAD_GRAYSCALE)  # グレースケールで読み込む
-                return img
-
-    def exportImage(self, fileName: str, processed_img: np.ndarray) -> None:
-        cv2.imwrite(os.path.join(self.outputFolderPath, fileName), processed_img)
+from postProcessing import PostProcessing
 
 class K20114(PostProcessing):
 
