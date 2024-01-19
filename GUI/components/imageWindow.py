da4fb0dd28f84c24d7ac984396a469a03f7b821c
Ayano <k22006kk@aitech.ac.jp>
2024-01-19 10:57:47 +0900

    feat: フォルダに画像ない場合の処理と画像サイズを指定できるよう修正

--- GUI/components/imageWindow.py
+++ GUI/components/imageWindow.py
@@ -5,31 +5,43 @@ import os
 
 # 画像を表示するウィンドウのクラス
 class ImageWindow(QLabel):
-    def __init__(self, filename=None, parent=None):
+    # 画像のサイズはデフォルトで800x600
+    def __init__(self,parent:QWidget | None=None,width:int=800,height:int=600):
         super(ImageWindow, self).__init__(parent)
-        if filename:
-            self.imagePath=filename
-            self.setImage(filename)
+        self.imagePath=None
+        self.width=width
+        self.height=height
+        self.updateImage()
 
     def setImage(self, filename=None):
         if filename:
             self.imagePath=filename
         self.Image=QtGui.QImage(self.imagePath)
-        # 写真のサイズが800*600になるように元のアスペクト比を保ったままリサイズ
-        self.Image=self.Image.scaled(800,600,QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+        # 写真のサイズが規定のサイズになるように元のアスペクト比を保ったままリサイズ
+        self.Image=self.Image.scaled(self.width,self.height,QtCore.Qt.AspectRatioMode.KeepAspectRatio)
         self.setPixmap(QtGui.QPixmap.fromImage(self.Image))
 
+    def resizeImage(self,width:int,height:int):
+        self.width=width
+        self.height=height
+        self.updateImage()
+
+    def updateImage(self):
+        folderPath="./img/edit/"
+        filename=[fileName for fileName in os.listdir(folderPath) if fileName!=".gitignore" and fileName!=".DS_Store" ]
+        if len(filename)>0:
+            self.imagePath=folderPath+filename[0]
+            self.setImage()
+        else:
+            # print("画像が見つかりませんでした")
+            self.setText("画像が見つかりませんでした")
 
 class DebugWindow(QWidget):
     def __init__(self, parent=None):
         super(DebugWindow, self).__init__(parent)
         self.setWindowTitle("デバッグ用ウィンドウ")
         self.resize(800, 600)
-        folderPath="./img/edit/"
-        filename=[fileName for fileName in os.listdir(folderPath) if fileName!=".gitignore" and fileName!=".DS_Store" ][0]
-        print(filename)
-        filePath=folderPath+filename
-        self.imageWindow=ImageWindow(filename=filePath,parent=self)
+        self.imageWindow=ImageWindow(parent=self)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
