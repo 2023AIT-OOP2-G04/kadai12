6b1b419b936a0c42b455299c376ae512e1eeffa6
Ryusan <x22037xx@aitech.ac.jp>
2024-01-22 15:23:27 +0900

    各ファイルの連結
    各ファイルのインポート
    各クラスのインスタンス化のみを行った
    実装確認などはしてない

--- module/ppManager.py
+++ module/ppManager.py
@@ -1,10 +1,21 @@
 # 各画像処理関数が書いてあるファイルのクラスをインポート
-from sample import k20000 as Sample 
+from sample import k20000 as Sample
+from k20114 import K20114 as K20114
+from k22025 import K22025 as K22025
+from k22136 import k22136 as K22136
+from k22136_2 import k22136 as k22136_2
+from x22037 import x22037 as X22037
 
-class PPManager():
+
+class PPManager:
     def __init__(self):
         # ここで各クラスをインスタンス化する
         self.sample = Sample()
+        self.k20114 = K20114()
+        self.k22025 = K22025()
+        self.k22136 = K22136()
+        self.k22136_2 = k22136_2()
+        self.x22037 = X22037()
         pass
 
 
