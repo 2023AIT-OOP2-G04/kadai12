517d4893e23664ce028861feda160c0a5e60bb0e
Ayano <k22006kk@aitech.ac.jp>
2024-01-26 07:44:42 +0900

    Revert "途中保存"
    This reverts commit 48bbe915e762f05f8ae2e526882c79c43c153c0c.

--- GUI/topPage.py
+++ GUI/topPage.py
@@ -3,18 +3,15 @@ import sys
 from PySide6.QtWidgets import *
 from PySide6 import QtGui,QtCore
 
-from components.scroll import ScrollArea
-from components.IshikawaLayoutTopPgae1 import TopPage
-
 class TopPage(QWidget):
-    def __init__(self,parent=None):
-        super().__init__(parent=parent)
-        
-        self.scrollArea = ScrollArea(self)  # スクロールエリアを作成
-        self.layout = QVBoxLayout(self) # レイアウトを作成
-        self.layout.addWidget(self.scrollArea)  # レイアウトにスクロールエリアを追加
-          # スクロールエリアに追加するウィジェットを作成
-        self.scrollArea.addWidget(TopPage(self)) # スクロールエリアにウィジェットを追加
+    def __init__(self):
+        super().__init__()
+        # 以下はテスト
+        self.button = QPushButton("Click me")
+        self.button.clicked.connect(lambda x: print("Hello! from TopPage"))
+        layout = QHBoxLayout()
+        layout.addWidget(self.button)
+        self.setLayout(layout)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
