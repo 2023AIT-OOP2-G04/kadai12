26e867be6c0fefc758f624e21c7362e522b38895
Ayano <k22006kk@aitech.ac.jp>
2024-01-26 08:25:22 +0900

    feat: 画像を２列で表示

--- GUI/components/IshikawaLayoutTopPgae1.py
+++ GUI/components/IshikawaLayoutTopPgae1.py
@@ -10,9 +10,14 @@ class LayoutTopPage(QWidget):
         super().__init__(parent=parent)
         self.setWindowTitle('トップページ') # ウィンドウのタイトル
         self.setGeometry(100,100,1600,1200) # ウィンドウの位置(x,y)と大きさ(w,h)
-        self.names = []
+        self.fileNames = []
+        self.folderPath = "./img/saved/"
+        self.getFileNames()
+        self.initUI()
 
 
+    def initUI(self):
+        
         # メッセージボックスを作成
         message = QLabel("ここにメッセージを書きます。",alignment = QtCore.Qt.AlignCenter)
         message.setFixedSize(300,200)
@@ -31,29 +36,6 @@ class LayoutTopPage(QWidget):
         layout_download = QHBoxLayout()
         layout_download.addWidget(self.button)
 
-
-        #画像の表示関数
-        def showImages(name):
-            beforeimg = QImage(name)
-            afterimg = beforeimg.scaled(300,500,QtCore.Qt.AspectRatioMode.KeepAspectRatio)
-
-            image = QLabel()
-            image.setPixmap(QPixmap.fromImage(afterimg))
-            layout_image = QHBoxLayout()
-            layout_image.addWidget(image,alignment=QtGui.Qt.AlignHCenter)
-            return layout_image
-        
-        #フォルダ内のファイル名を取得
-        def showFilesName(path) :
-             for fileName in os.listdir(path):
-                 if fileName != ".gitignore":
-                    if fileName != ".DS_Store":
-                        print(fileName)
-                        self.names.append(fileName)
-                        layout_image = QHBoxLayout()
-                        layout_image.addLayout(showImages(path+fileName))
-                        self.layout.addLayout(layout_image)
-        
         layout_buttons = QHBoxLayout()
         layout_buttons.addLayout(layout_upload)
         layout_buttons.addLayout(layout_download)
@@ -61,22 +43,46 @@ class LayoutTopPage(QWidget):
         self.layout = QVBoxLayout()
         self.layout.addLayout(layout_message)
         self.layout.addLayout(layout_buttons)
+        self.layoutImages1 = QVBoxLayout()
+        self.layoutImages2 = QVBoxLayout()
 
-        showFilesName("./img/saved/")
+        for count,fileName in enumerate(self.fileNames):
+            if count%2==0:
+                 self.layoutImages1.addLayout(self.showImage(self.folderPath+fileName))
+                #  print(f"1:{fileName}")
+            else:
+                self.layoutImages2.addLayout(self.showImage(self.folderPath+fileName))
+                # print(f"2:{fileName}")
+        self.layoutImages=QHBoxLayout()
+        self.layoutImages.addLayout(self.layoutImages1)
+        self.layoutImages.addLayout(self.layoutImages2)
 
-        # layout_v = QVBoxLayout() 
-        # layout_h = QHBoxLayout()
-        # for i in range(len(self.names)):
-        #     if i%2 == 1:
-        #         showFilesName("./img/saved/")
-        #     else:
-        #         showFilesName("./img/saved/")
-        # # layout_images = QHBoxLayout()
-        # layout_images.addLayout(layout_v)
-        # layout_images.addLayout(layout_h)
+        self.layout.addLayout(self.layoutImages)
 
-        # self.layout.addLayout(showFilesName("./img/edit/"))
+        
         self.setLayout(self.layout)
+
+
+    #画像の表示関数
+    def showImage(self,imagePath:str) -> QHBoxLayout:
+        beforeimg = QImage(imagePath)
+        afterimg = beforeimg.scaled(300,500,QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+
+        image = QLabel()
+        image.setPixmap(QPixmap.fromImage(afterimg))
+        layout_image = QHBoxLayout()
+        layout_image.addWidget(image,alignment=QtGui.Qt.AlignHCenter)
+        return layout_image
+        
+    #フォルダ内のファイル名を取得
+    def getFileNames(self) :
+            # savedフォルダ内から.gitignoreと.DS_Storeを除いたファイル名の配列を取得
+            self.fileNames = [f for f in os.listdir(self.folderPath) if os.path.isfile(os.path.join(self.folderPath, f)) and f != ".gitignore" and f != ".DS_Store"]
+            # print(self.fileNames)
+                
+                        
+        
+        
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 
 if __name__ == "__main__":
