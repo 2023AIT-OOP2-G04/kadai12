17dba091f9a0d69b04fe79fdf91afab1d97fa13e
moridokin <mayumi24319@icloud.com>
2024-01-25 14:39:45 +0900

    perf:色変更と縦横比変更

--- module/k22136.py
+++ module/k22136.py
@@ -10,12 +10,12 @@ class k22136(PostProcessing):
         super().__init__()
         pass
 
-    def adjust_saturation_and_brightness(self, input_path, output_path, saturation_factor, brightness_factor):
+    def adjust_saturation_and_brightness(self, saturation_factor, brightness_factor):
         # 画像を開く
-        original_image = cv2.imread(input_path)
-
+        image = self.getEditImage()
+        
         # 彩度と明度を変更
-        hsv_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         hsv_image[:, :, 1] = np.clip(hsv_image[:, :, 1] * saturation_factor, 0, 255)
         hsv_image[:, :, 2] = np.clip(hsv_image[:, :, 2] * brightness_factor, 0, 255)
 
@@ -23,22 +23,16 @@ class k22136(PostProcessing):
         adjusted_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
         
         # 画像を保存
-        self.save_img(hsv_image)
-
-    # 同じファイル名でeditフォルダ内の画像を上書き保存する関数、継承すれば使えるようになります
-    def saveImage(self, img: cv2.Mat) -> None:
-        cv2.imwrite(self.image_path, img)
+        self.saveImage(adjusted_image)
 
 if __name__ == "__main__":
 
     pp = k22136()
 
     # ユーザーからの入力を受け取る  
-    image_path = "img/edit/iro_input.jpg"
-    output_path = "img/edit/iro_input.jpg"
     saturation_factor = float(input("彩度の変更倍率を入力してください (1.0 が変更なし): "))
     brightness_factor = float(input("明度の変更倍率を入力してください (1.0 が変更なし): "))
 
     # 関数を呼び出して画像の彩度と明度を変更
-    pp.adjust_saturation_and_brightness(image_path, output_path, saturation_factor, brightness_factor)
+    pp.adjust_saturation_and_brightness(saturation_factor, brightness_factor)
 
--- module/k22136_2.py
+++ module/k22136_2.py
@@ -10,12 +10,12 @@ class k22136_2(PostProcessing):
         super().__init__()
         pass
 
-    def resize_image(self,input_path, output_path, target_aspect_ratio):
+    def resize_image(self, target_aspect_ratio):
         # 画像を開く
-        original_image = cv2.imread(input_path)
+        image = self.getEditImage()
 
         # 元の画像の縦横比を計算
-        original_height, original_width, _ = original_image.shape
+        original_height, original_width, _ = image.shape
         original_aspect_ratio = original_width / original_height
 
         # ターゲットの縦横比に合わせて新しいサイズを計算xs
@@ -27,14 +27,11 @@ class k22136_2(PostProcessing):
             new_height = int(original_width / target_aspect_ratio)
 
         # 画像をリサイズ
-        resized_image = cv2.resize(original_image, (new_width, new_height))
+        resized_image = cv2.resize(image, (new_width, new_height))
 
         # リサイズされた画像を保存
-        self.save_img(resized_image)
+        self.saveImage(resized_image)
 
-    # 同じファイル名でeditフォルダ内の画像を上書き保存する関数、継承すれば使えるようになります
-    def saveImage(self, img: cv2.Mat) -> None:
-        cv2.imwrite(self.image_path, img)
     
 # debug用
 if __name__ == "__main__":
@@ -44,9 +41,5 @@ if __name__ == "__main__":
     # ユーザーによって指定された縦横比を取得
     user_aspect_ratio = float(input("縦横比を入力してください（例: 1.77）: "))
 
-    # 例: 入力画像のパスと出力画像のパス
-    image_path = "img/edit/tateyoko_input.jpg"
-    output_path = "img/edit/tateyoko_input.jpg"
-
     # 画像を指定された縦横比にリサイズ
-    pp.resize_image(image_path, output_path, user_aspect_ratio) 
+    pp.resize_image(user_aspect_ratio) 
